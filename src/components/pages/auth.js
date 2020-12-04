bd942c69c03ca143453186acf198fd9a5c28e54a
Markus George <gmarkus993@gmail.com>
2020-12-04 05:40:58 -0600

    Added edit and clear function to portfolio manager

---
+++ src/components/pages/auth.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Login from "../auth/login";
+import loginImage from "../../../static/assets/images/auth/login.jpg"
+
+
+export default class auth extends Component {
+    constructor(props){
+        super(props)
+
+        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+        this.handleUnSuccessfulAuth = this.handleUnSuccessfulAuth.bind(this);
+    }
+
+    handleSuccessfulAuth() {
+        this.props.handleSuccessfulLogin();
+        this.props.history.push("/");
+    }
+
+    handleUnSuccessfulAuth(){
+        this.props.handleSuccessfulLogin();
+    }
+
+
+    render() {
+        return (
+            <div className="auth-page-wrapper">
+                <div className="left-column"
+                style={{
+                    backgroundImage: `url(${loginImage})`,
+                    
+                }}
+                />
+
+                <div className="right-column">
+                    <Login
+                    handleSuccessfulAuth={this.handleSuccessfulAuth}
+                    handleUnSuccessfulAuth={this.handleUnSuccessfulAuth}
+                    />
+                </div>
+
+            </div>
+        )
+    }
+}
---
+++ src/components/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PortfolioConainer from "../portfolio/portfolio-container"
+export default function(){
+    return(
+        <div>
+            
+            <PortfolioConainer/>
+        </div>
+    )
+}
